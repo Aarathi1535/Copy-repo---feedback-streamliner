50547870a89fcec5a3235510fa8add8234f00609
Balla Aarathisree <121624731+Aarathi1535@users.noreply.github.com>
2026-02-14 13:17:03 +0530

    Update FeedbackReport.tsx

--- components/FeedbackReport.tsx
+++ components/FeedbackReport.tsx
@@ -95,7 +95,6 @@ const FeedbackReport: React.FC<FeedbackReportProps> = ({ report }) => {
               </svg>
             </div>
           </div> */}
-        </div>
       </div>
 
       {/* Metadata Section */}
